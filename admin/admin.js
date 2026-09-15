e0003e7c87d8de2edd7451ba2177b283b874c495
Kobby Junior <kobbyjunior56@gmail.com>
2026-09-15 08:34:42 +0000

    Link change

--- admin/admin.js
+++ admin/admin.js
@@ -1,4 +1,4 @@
-const API_URL = "https://a-and-o-beverages.onrender.com/api";
+const API_URL = "https://a-and-o-beverages.onrender.com";
 
 const loginScreen = document.getElementById("loginScreen");
 const dashboardScreen = document.getElementById("dashboardScreen");

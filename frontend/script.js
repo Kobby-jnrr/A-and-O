a6a17e9a719d32fb46b3960bda50b29da45544e7
Kobby Junior <kobbyjunior56@gmail.com>
2026-09-17 11:30:18 +0000

    Fetch Problem Fixed

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@
    Main frontend JavaScript
 ========================================================= */
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://a-and-o-beverages.onrender.com/api";
 const CURRENCY = "GHS";
 
 const MAX_PARFAIT_FRUITS = 3;
